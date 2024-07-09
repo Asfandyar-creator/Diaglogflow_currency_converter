17520b1fda5c643807536ef85963a90c5ca68c7e
Asfandyar <yar69416@gmail.com>
2024-07-09 14:43:47 +0500

    Refactor currency conversion logic to use f-strings for improved readability

--- app.py
+++ app.py
@@ -6,27 +6,28 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def index():
     data = request.get_json()
-    source_currency = data['queryResult']['parameters']['unit-currency'][0]['currency']
-    amount = data['queryResult']['parameters']['unit-currency'][0]['amount']
-    target_currency = data['queryResult']['parameters']['currency-name'][0]
-
+    source_currency = data['queryResult']['parameters']['unit-currency']['currency']
+    amount = data['queryResult']['parameters']['unit-currency']['amount']
+    target_currency = data['queryResult']['parameters']['currency-name']
 
     cf = fetch_conversion_factor(source_currency, target_currency)
     final_amount = amount * cf
     final_amount = round(final_amount, 2)
-    print(final_amount)
     response = {
-        "fulfillmentText": "{} {} is {} {}".format(amount, source_currency, final_amount, target_currency)
+        "fulfillmentText": f"{amount} {source_currency} is {final_amount} {target_currency}"
     }
     return jsonify(response)
 
 def fetch_conversion_factor(source, target):
-    # logic to fetch conversion factor from a database or API
-    url = 'https://v6.exchangerate-api.com/v6/c36fea12f9bd825c1923be2a/pair/{}/{}'.format(source, target)
+    api_key = 'c36fea12f9bd825c1923be2a'  # Replace with your actual API key
+    url = f'https://v6.exchangerate-api.com/v6/{api_key}/pair/{source}/{target}'
     response = requests.get(url)
-    response = response.json()
-    return response['conversion_rate']
+    response.raise_for_status()  # Raise an exception for HTTP errors
+    return response.json()['conversion_rate']
 
+@app.route('/', methods=['GET'])
+def home():
+    return "Currency conversion service is running. Use POST method to convert currency."
 
 if __name__ == '__main__':
     app.run(debug=True)
